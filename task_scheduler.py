efea432c6f412c9bcdb670246eb80f7a1ae95475
= <=>
2023-07-12 02:04:24 +0000

    Task Scheduler (621)

---
+++ task_scheduler.py
@@ -0,0 +1,17 @@
+from collections import Counter
+
+class Solution:
+    @staticmethod
+    def leastInterval(tasks: list[str], n: int) -> int:
+        counter: Counter[str] = Counter(tasks)
+
+        max_freq: int = counter.most_common(1)[0][1]
+
+        return max(
+            (max_freq - 1) * (n + 1) + list(counter.values()).count(max_freq),
+            len(tasks)
+        )
+
+if __name__ == '__main__':
+    print(Solution().leastInterval(["A","A","A","B","B","B"], 2))
+    # print(Solution().leastInterval(["A","A","A","B","B","B"], 0))
